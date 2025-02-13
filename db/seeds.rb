d8e43b2534ba22071a1d33e21e63338bcfe0910f
Thomas Morin <thomasmorin@MacBook-Pro-de-Thomas.local>
2025-02-13 13:42:32 +0100

    Add blocs data and seeds update-create method

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,28 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'json'
+
+#occi_blocs
+json_text = File.read(Rails.root.join('db','occi_blocs.json'))
+JSON.parse(json_text).each do |occi_bloc|
+  bloc = Bloc.find_by(code: occi_bloc['code'])
+  if bloc
+  	bloc.update!(occi_bloc)
+  else
+   Bloc.create!(occi_bloc)
+  end
+end
+puts Bloc.where(jap: false).count
+
+#jap_blocs
+json_text = File.read(Rails.root.join('db','jap_blocs.json'))
+JSON.parse(json_text).each do |jap_bloc|
+  bloc = Bloc.find_by(code: jap_bloc['code'])
+  if bloc
+  	bloc.update!(jap_bloc)
+  else
+   Bloc.create!(jap_bloc)
+  end
+end
+puts Bloc.where(jap: true).count
