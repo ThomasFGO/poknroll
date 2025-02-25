3339951fa059275d61eb6650c196610bf00b317c
Thomas Morin <thomasmorin@MacBook-Pro-de-Thomas.local>
2025-02-25 18:27:44 +0100

    Add two expansions symbols

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,9 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 require 'json'
+require 'csv'
+
+
 
 #occi_blocs
 json_text = File.read(Rails.root.join('db','occi_blocs.json'))
@@ -38,7 +41,22 @@ json_text = File.read(Rails.root.join('db', 'occi_lists','sv.json'))
 JSON.parse(json_text).each do |expansion|
   existing_expansion = Expansion.find_by(code: expansion['code'])
   if existing_expansion
-  	existing_expansion.update!(expansion)
+  	existing_expansion.update!(rank: expansion['rank'],
+   	code: expansion['code'],   	
+   	fr_name: expansion['fr_name'],   	
+   	fr_release: expansion['fr_release'],  	
+   	en_name: expansion['en_name'],
+   	us_release: expansion['us_release'],
+   	jap_name: expansion['jap_name'],
+   	jap_release: expansion['jap_release'],
+   	classic_size: expansion['classic_size'],
+   	full_size: expansion['full_size'],
+   	category: expansion['category'],
+   	fr_logo_url: expansion['fr_logo_url'],
+   	en_logo_url: expansion['en_logo_url'],
+   	symbol_url: expansion['symbol_url'],
+   	bloc: Bloc.find_by(code: expansion['bloc_code'])
+  		)
   else
    Expansion.create!(rank: expansion['rank'],
    	code: expansion['code'],   	
@@ -60,3 +78,14 @@ JSON.parse(json_text).each do |expansion|
 end
 puts Expansion.count
 
+=begin
+
+csv_text = File.read(Rails.root.join('db', 'occi_lists', 'ss.csv'))
+csv = CSV.parse(csv_text, col_sep: ';', headers: :first_row, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  code = row['code']
+  symbol_url = row['symbol_url']
+  Cloudinary::Uploader.upload("#{symbol_url}",:public_id => "#{code}",:folder => "lists/symbols")
+end
+
+=end
